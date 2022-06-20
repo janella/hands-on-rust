41b332fbb92197f03901b5ee287b386f5f676b74
janella <694610+janella@users.noreply.github.com>
2022-06-20 18:46:47 +1200

    feat: camera impl

--- 2_roguelike/src/map.rs
+++ 2_roguelike/src/map.rs
@@ -22,21 +22,27 @@ impl Map {
         }
     }
 
-    pub fn render(&self, ctx: &mut BTerm) {
-        for y in 0..SCREEN_HEIGHT {
-            for x in 0..SCREEN_WIDTH {
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+        ctx.set_active_console(0); // where is this 0 from?
+        for y in camera.top_y..camera.bottom_y {
+            for x in camera.left_x..camera.right_x {
                 let idx = get_map_idx(x, y);
-                // Original:
-                // match self.tiles[idx] {
-                //     TileType::Floor => ctx.set(x, y, YELLOW, BLACK, to_cp437('.')),
-                //     TileType::Wall => ctx.set(x, y, DARK_GRAY, BLACK, to_cp437('#')),
-                // }
-
-                // J: use `get`
                 if let Some(tile) = self.tiles.get(idx) {
                     match tile {
-                        TileType::Floor => ctx.set(x, y, YELLOW, BLACK, to_cp437('.')),
-                        TileType::Wall => ctx.set(x, y, DARK_GRAY, BLACK, to_cp437('#')),
+                        TileType::Floor => ctx.set(
+                            x - camera.left_x,
+                            y - camera.top_y,
+                            WHITE,
+                            BLACK,
+                            to_cp437('.'),
+                        ),
+                        TileType::Wall => ctx.set(
+                            x - camera.left_x,
+                            y - camera.top_y,
+                            DARK_GRAY,
+                            BLACK,
+                            to_cp437('#'),
+                        ),
                     }
                 }
             }
