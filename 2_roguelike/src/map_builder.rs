e88571a9563db864aded47c09d68ddf247c97b83
janella <694610+janella@users.noreply.github.com>
2022-06-20 18:24:08 +1200

    feat: mapbuilder implementation
    Seems to have a bug where rooms don't correctly link up to each other.
    Sometimes copypasta doesn't work :) Time to debug.

--- 2_roguelike/src/map_builder.rs
+++ 2_roguelike/src/map_builder.rs
@@ -9,6 +9,19 @@ pub struct MapBuilder {
 }
 
 impl MapBuilder {
+    pub fn new(rng: &mut RandomNumberGenerator) -> Self {
+        let mut mb = Self {
+            map: Map::new(),
+            rooms: Vec::new(),
+            player_start: Point::zero(),
+        };
+        mb.fill(TileType::Wall);
+        mb.build_random_rooms(rng);
+        mb.build_corridors(rng);
+        mb.player_start = mb.rooms[0].center();
+        mb
+    }
+
     fn fill(&mut self, tile: TileType) {
         // q: what the heck are we doing here!
         // * is a dereference - t is a reference &TileType
@@ -43,6 +56,43 @@ impl MapBuilder {
                     }
                 });
                 self.rooms.push(room);
+            }
+        }
+    }
+
+    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
+        use std::cmp::{max, min}; // q: local import??
+        for y in min(y1, y2)..=max(y1, y2) {
+            // ..= is range inclusive
+            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
+                self.map.tiles[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
+        use std::cmp::{max, min}; // q: local import??
+        for x in min(x1, x2)..=max(x1, x2) {
+            // ..= is range inclusive
+            if let Some(idx) = self.map.try_idx(Point::new(x, y)) {
+                self.map.tiles[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn build_corridors(&mut self, rng: &mut RandomNumberGenerator) {
+        let mut rooms = self.rooms.clone();
+        rooms.sort_by(|a, b| a.center().x.cmp(&b.center().x));
+
+        for (i, room) in rooms.iter().enumerate().skip(1) {
+            let prev = rooms[i - 1].center();
+            let new = room.center();
+            if rng.range(0, 2) == 1 {
+                self.apply_horizontal_tunnel(prev.x, new.x, prev.y);
+                self.apply_vertical_tunnel(prev.y, new.y, new.x);
+            } else {
+                self.apply_vertical_tunnel(prev.y, new.y, prev.x);
+                self.apply_horizontal_tunnel(prev.x, new.x, new.y);
             }
         }
     }
