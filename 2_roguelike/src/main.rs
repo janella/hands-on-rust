2eba9b447e213447d95a0aa67158a4790b3da84c
janella <694610+janella@users.noreply.github.com>
2022-06-22 16:20:51 +1200

    feat: hud, health

--- 2_roguelike/src/main.rs
+++ 2_roguelike/src/main.rs
@@ -72,7 +72,12 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
+        ctx.set_active_console(2);
+        ctx.cls();
         self.resources.insert(ctx.key);
+        ctx.set_active_console(0);
+        self.resources.insert(Point::from_tuple(ctx.mouse_pos()));
+
         // q: "we know for sure a turn state exists - skip error checking"
         // _we_ know, but how does the program know?
         // almost got `if let` working, to avoid explicit unwrap and possible
@@ -102,8 +107,10 @@ fn main() -> BError {
         .with_tile_dimensions(32, 32)
         .with_resource_path("resources/")
         .with_font("dungeonfont.png", 32, 32)
+        .with_font("terminal8x8.png", 8, 8)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, "terminal8x8.png")
         .build()?;
     main_loop(context, State::new())
 }
