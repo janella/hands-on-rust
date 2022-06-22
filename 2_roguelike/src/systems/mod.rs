29d28ab7bb0e9acc855d3b745455d7ba7c280cc2
janella <694610+janella@users.noreply.github.com>
2022-06-22 18:45:17 +1200

    ch8: scrap collision detection

--- 2_roguelike/src/systems/mod.rs
+++ 2_roguelike/src/systems/mod.rs
@@ -1,5 +1,4 @@
 use crate::prelude::*;
-mod collisions;
 mod end_turn;
 mod entity_render;
 mod hud;
@@ -24,8 +23,6 @@ pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(collisions::collisions_system())
-        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(hud::hud_system())
@@ -38,8 +35,6 @@ pub fn build_monster_scheduler() -> Schedule {
         .add_system(random_move::random_move_system())
         .flush()
         .add_system(movement::movement_system())
-        .flush()
-        .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
